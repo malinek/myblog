716119d958edda7c68b00bb24297a0db38b641ac
malinek <malin.ekelund@gmail.com>
2018-02-22 15:31:00 +0100

    jumbotron image

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  
+  root 'posts#index'
+
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
-  root 'posts#index'
+  get 'posts/index'
+
+  
 
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
