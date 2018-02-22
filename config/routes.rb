9733fab7220d288a468fa80a91c1e086e3d5708d
malinek <malin.ekelund@gmail.com>
2018-02-22 17:15:53 +0100

    assets:precompile

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,6 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
 
-  get 'posts/index'
-
-  
-
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
