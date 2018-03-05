05b2b084e4cbcff5fb3a0042e0e20315d07e99a0
malinek <malin.ekelund@gmail.com>
2018-03-05 16:10:52 +0100

    add comments model & controller & views

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,27 @@
+class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @post, notice: 'Comment was created successfully'}
+        format.json {render :show, status: :created, location: @post }
+      else
+        format.html { redirect_to @post, alert: 'Comment was not saved successfully' }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:name, :body)
+    end
+
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,11 @@
 class Post < ApplicationRecord
+  validates :title, presence:true
+  has_many :comments
   acts_as_taggable
 
   def self.search(search_term)
-  	like_string = Rails.env.production? ? "ilike" : "LIKE"
-  	Post.where("title LIKE ?", "%#{search_term}%")
+    like_string = Rails.env.production? ? "ilike" : "LIKE"
+    Post.where("title LIKE ?", "%#{search_term}%")
   end
 
 end
