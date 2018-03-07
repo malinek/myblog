e9d46c60376d68429ed157e0443ecb66a7f38ebd
malinek <malin.ekelund@gmail.com>
2018-03-07 11:16:59 +0100

    edit search function, styling search field

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,11 @@ class Post < ApplicationRecord
   acts_as_taggable
 
   def self.search(search_term)
-    like_string = Rails.env.production? ? "ilike" : "LIKE"
-    Post.where("title LIKE ?", "%#{search_term}%")
+    if Rails.env.production?
+      Post.where("title ilike ?", "%#{search_term}%")
+    else
+      Post.where("title LIKE ?", "%#{search_term}%")
+    end
   end
 
 end
