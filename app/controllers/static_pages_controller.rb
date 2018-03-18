e6b6bb76c15887c94514ea347d6957cb9a7cd96b
malinek <malin.ekelund@gmail.com>
2018-03-18 11:40:54 +0800

    add sendgrid, edit links footer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,12 @@
 class StaticPagesController < ApplicationController
   def about
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email, to:'malin.ekelund@gmail.com', subject: "A new contact message from #{@name}", body: @message).deliver_now
+  end
+
 end
