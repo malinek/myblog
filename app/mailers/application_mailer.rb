0a36173065bf794ef4311263fa913b6ef8dc6175
malinek <malin.ekelund@gmail.com>
2018-04-29 12:01:57 +0200

    edit contact form

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,12 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+
+  def contact_form(email, name, message)
+  @message = message
+    mail(from: email,
+         to: 'malin.ekelund@gmail.com',
+         subject: "A new contact form message from #{name}")
+  end
+
 end
